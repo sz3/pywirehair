1cdd7fae217d05edea2ff471b566bee4374a7eef
sz <sz@galacticicecube.com>
2020-11-18 02:24:50 -0600

    oops

--- pywirehair/wirehair.py
+++ pywirehair/wirehair.py
@@ -4,7 +4,6 @@ from os.path import join as path_join, abspath, dirname
 from .constants import Wirehair_Success
 
 _dll_path = path_join(dirname(abspath(__file__)), 'libwirehair.so')
-_wirehair = ctypes.CDLL(_dll_path)
 
 
 class wirehair:
@@ -12,17 +11,16 @@ class wirehair:
     _init = False
 
     def __new__(cls, *args, **kwargs):
+        # do wirehair init stuff on first run
         if cls._wirehair == None:
             cls._wirehair = ctypes.CDLL(_dll_path)
         if cls._init == False:
-            print('init?')
-            cls._init = (_wirehair.wirehair_init_(2) == Wirehair_Success)
+            cls._init = (cls._wirehair.wirehair_init_(2) == Wirehair_Success)
         return super().__new__(cls, *args, **kwargs)
 
     def dll(self):
-        return _wirehair
+        return self._wirehair
 
     def create_encoder(self, msg, packet_size):
-        print(_wirehair)
         from .encoder import encoder
         return encoder(msg, packet_size)

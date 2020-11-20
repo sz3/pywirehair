6911ba206ef0da206ac314ff1ee4c6c08397a853
sz <sz@galacticicecube.com>
2020-11-20 03:13:01 -0600

    Fix encoder, add basic test

--- pywirehair/encoder.py
+++ pywirehair/encoder.py
@@ -6,11 +6,11 @@ from .wirehair import wirehair
 
 class encoder:
     def __init__(self, msg, packet_size):
-        msg = (ctypes.c_uint8 * len(msg)).from_buffer_copy(msg)
-        self._eid = wirehair().dll().wirehair_encoder_create(
-            0, ctypes.byref(msg), ctypes.c_uint64(len(msg)), ctypes.c_uint32(packet_size)
-        )
+        self.msg = (ctypes.c_uint8 * len(msg)).from_buffer_copy(msg)
         self.packet_size = packet_size
+        self._eid = wirehair().dll().wirehair_encoder_create(
+            0, ctypes.byref(self.msg), ctypes.c_uint64(len(self.msg)), ctypes.c_uint32(packet_size)
+        )
         self._buff = (ctypes.c_uint8 * packet_size)()
 
     def encode(self, block_id):
--- pywirehair/wirehair.py
+++ pywirehair/wirehair.py
@@ -8,19 +8,15 @@ _dll_path = path_join(dirname(abspath(__file__)), 'libwirehair.so')
 
 class wirehair:
     _wirehair = None
-    _init = False
+    _init = -1
 
     def __new__(cls, *args, **kwargs):
         # do wirehair init stuff on first run
         if cls._wirehair == None:
             cls._wirehair = ctypes.CDLL(_dll_path)
-        if cls._init == False:
-            cls._init = (cls._wirehair.wirehair_init_(2) == Wirehair_Success)
+        if cls._init != Wirehair_Success:
+            cls._init = cls._wirehair.wirehair_init_(2)
         return super().__new__(cls, *args, **kwargs)
 
     def dll(self):
         return self._wirehair
-
-    def create_encoder(self, msg, packet_size):
-        from .encoder import encoder
-        return encoder(msg, packet_size)

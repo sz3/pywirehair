4d6dea13aae175f28d1c0d7b248fc6530120f076
sz <sz@galacticicecube.com>
2020-11-30 13:40:47 -0600

    experiment

--- pywirehair/wirehair.py
+++ pywirehair/wirehair.py
@@ -3,7 +3,7 @@ from os.path import join as path_join, abspath, dirname
 
 from .constants import Wirehair_Success
 
-_dll_path = path_join(dirname(abspath(__file__)), 'libwirehair.so.2')
+_dll_path = path_join(dirname(abspath(__file__)), 'libwirehair.so')
 
 
 class wirehair:
@@ -14,8 +14,11 @@ class wirehair:
         # do wirehair init stuff on first run
         if cls._wirehair is None:
             cls._wirehair = ctypes.CDLL(_dll_path)
+            print(cls._wirehair)
         if cls._init != Wirehair_Success:
+            print('attempting init:')
             cls._init = cls._wirehair.wirehair_init_(2)
+            print(cls._wirehair)
         return super().__new__(cls, *args, **kwargs)
 
     def dll(self):

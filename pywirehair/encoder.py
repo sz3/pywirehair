698813df22eff982cf0fd90330f416a2168005f2
sz <sz@recv.cc>
2023-06-05 20:48:36 -0500

    Fix a typing bug in how we handle the `WirehairCodec` object
    It worked on older systems, but it was wrong.

--- pywirehair/encoder.py
+++ pywirehair/encoder.py
@@ -8,15 +8,17 @@ class encoder:
     def __init__(self, msg, packet_size):
         self.msg = (ctypes.c_uint8 * len(msg)).from_buffer_copy(msg)
         self.packet_size = packet_size
+        # WirehairCodec is a pointer, and we need to make that explicit
+        wirehair().wirehair_encoder_create.restype = ctypes.c_void_p
         self._eid = wirehair().wirehair_encoder_create(
-            0, ctypes.byref(self.msg), ctypes.c_uint64(len(self.msg)), ctypes.c_uint32(packet_size)
+            None, ctypes.byref(self.msg), ctypes.c_uint64(len(self.msg)), ctypes.c_uint32(packet_size)
         )
         self._buff = (ctypes.c_uint8 * packet_size)()
 
     def encode(self, block_id):
         writelen = ctypes.c_uint32(0)
         res = wirehair().wirehair_encode(
-            self._eid, ctypes.c_uint(block_id), ctypes.byref(self._buff), ctypes.c_uint32(self.packet_size),
+            ctypes.c_void_p(self._eid), ctypes.c_uint(block_id), ctypes.byref(self._buff), ctypes.c_uint32(self.packet_size),
             ctypes.byref(writelen)
         )
         if res != Wirehair_Success:
